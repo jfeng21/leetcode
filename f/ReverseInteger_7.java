a56a036078e075f023127697154c5777da7c9db1
jfeng21 <junru.feng@asu.edu>
2016-06-22 16:48:08 -0400

    Create ReverseInteger_7.java

---
+++ f/ReverseInteger_7.java
@@ -0,0 +1,13 @@
+class ReverseInteger_7{
+public int reverse(int x){
+int res = 0;
+while(x != 0){
+if(Math.abs(res) > 214748364)
+  return 0;
+res = res * 10 + x % 10;   //important!!!!
+x /= 10;
+}
+return res;
+}
+
+}
