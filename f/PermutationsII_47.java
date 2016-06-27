78696665b0f1602d2eebb7baaf41e6afa4c34a61
jfeng21 <junru.feng@asu.edu>
2016-06-27 13:08:12 -0400

    Create PermutationsII_47.java

---
+++ f/PermutationsII_47.java
@@ -0,0 +1,32 @@
+class PermutationsII_47{
+  public List<List<Integer>> permutateUnique(int[] nums){
+   List<List<Integer>> res = new ArrayList<List<Integer>>();
+    if(nums == null || nums.length == 0)
+      return res;
+   ArrayList<Integer> list = new ArrayList<>();
+   int[] visited = new int[nums.length];
+   Arrays.sort(nums);
+  helper(res, list, nums,visited);
+  return res;
+  
+  
+  }
+  
+  private void helper(List<List<Integer>> res, ArrayList<Integer> list, int[] nums, int[] visited){
+   if(list.size() == nums.length){
+     res.add(new ArrayList<>(list));
+     return;
+     }
+   for(int i = 0; i < nums.length; i++){
+     if(visited[i] == 1 || (i != 0 && nums[i] == nums[i - 1] && visited[i - 1] == 0))  //!!!!check if its visited
+       continue;
+    visited[i] = 1;
+    list.add(nums[i]);
+    helper(res, list, nums,visited);
+    list.remove(list.size() - 1);
+    visited[i] = 0;
+   }
+  
+  
+  }
+}
