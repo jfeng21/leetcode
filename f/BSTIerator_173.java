48269bd1f7b36e344d91cd0a28ea8053588fef5e
jfeng21 <junru.feng@asu.edu>
2016-06-28 12:38:02 -0400

    Create BSTIerator_173.java
    if input nodes are : 10, 7, 12, 5, 8, int next() returns 5..

---
+++ f/BSTIerator_173.java
@@ -0,0 +1,26 @@
+class BSTIerator_173{
+
+  private Stack<TreeNode> stack = new Stack<TreeNode>();
+  
+  public BSTIterator(TreeNode root){
+     pushAll(root);
+  }
+
+  public boolean hasNext(){
+    return !stack.isEmpty();
+  }
+  //next smallest number
+  public int next(){
+     TreeNode min = stack.pop();
+     pushAll(min.right);
+     return min.val;
+  }
+  
+  private void pushAll(TreeNode node){
+    while(node != null){
+        stack.push(node);
+        node = node.left;
+      }
+  }
+
+}
