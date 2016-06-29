2da9f8b4b16d23ec0c00d558c049603437565b58
jfeng21 <junru.feng@asu.edu>
2016-06-29 15:54:03 -0400

    Create BestTimeBuyStock_121.java

---
+++ f/BestTimeBuyStock_121.java
@@ -0,0 +1,18 @@
+class BestTimeBuyStock_121{
+  public int maxProfit(int[] prices){
+    if(prices == null || prices.length == 0)
+      return 0;
+    int price = prices[0];  //track min price
+    int maxProfit = 0;
+    
+    for(int i = 0; i < prices.length; i++){
+     if(price > prices[i])
+       price = prices[i];
+    else{
+       int pro = prices[i] - price;
+      maxProfit = Math.max(maxProfit, pro);
+    }
+  }
+  return maxProfit;
+}
+}
