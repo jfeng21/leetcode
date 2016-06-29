f4e12d79410014c835fe3a4b1ce66fd7b7895500
jfeng21 <junru.feng@asu.edu>
2016-06-29 14:15:41 -0400

    Update FindPeakElement_162.java

--- f/FindPeakElement_162.java
+++ f/FindPeakElement_162.java
@@ -6,7 +6,7 @@ class FindPeakElement_162{
       
       while(left < right){
         int mid = left + (right - left) / 2;
-        if(nums[mid] < nums[mid + 1])
+        if(nums[mid] < nums[mid + 1])  //rising are, move right
           left = mid + 1;
         else
           right = mid;
