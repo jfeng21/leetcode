51339ebc1b9c85d99c59e587f5f37637527a55e9
jfeng21 <junru.feng@asu.edu>
2016-06-29 14:13:35 -0400

    Create FindPeakElement_162.java

---
+++ f/FindPeakElement_162.java
@@ -0,0 +1,19 @@
+class FindPeakElement_162{
+//binary search
+  public int findPeakElement(int[] nums){
+      if(nums.length == 1)  return 0;
+      int left = 0, right = nums.length - 1;
+      
+      while(left < right){
+        int mid = left + (right - left) / 2;
+        if(nums[mid] < nums[mid + 1])
+          left = mid + 1;
+        else
+          right = mid;
+      
+      }
+      return left;  
+    
+  }
+
+}
