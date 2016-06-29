2ed1caed9011db9fccc5b0f0edf09826d27349d0
jfeng21 <junru.feng@asu.edu>
2016-06-29 12:08:12 -0400

    Create LongestIncreasingSubsequence_300.java

---
+++ f/LongestIncreasingSubsequence_300.java
@@ -0,0 +1,20 @@
+class LongestIncreasingSubsequence_300{
+//dp. O(n)
+  public int lengthOfLIS(int[] nums){
+      if(nums.length == 0)  return 0;
+      int[] a = new int[nums.length];
+      int max = 0;
+      for(int i = 0; i < nums.length; i++){
+        a[i] = 1;  //initialize, longest subsequence at least one
+        for(int j = 0; j < i; j++){
+          if(nums[i] > nums[j]){
+             a[i] = a[i] > a[j] + 1 ? a[i] : a[j] + 1;
+          }
+        }
+        if(a[i] > max)
+           max = a[i];
+      }
+    return max;
+  }
+
+}
