9404ed1de40a444375d214d918af02e65792eaa4
jfeng21 <junru.feng@asu.edu>
2016-06-29 20:06:09 -0400

    Create CountandSay_38.java

---
+++ f/CountandSay_38.java
@@ -0,0 +1,23 @@
+class CountandSay_38{
+  public String countAndSay(int n){
+    String res = "1";
+    for(int i = 1; i < n; i++){
+     String prev = res;
+     res = "";
+     int count = 1;
+     char say = prev.charAt(0);
+       for(int j = 1; j < prev.length(); j++){
+         if(prev.charAt(j) != say){
+           res = res + count + say;
+           count = 1;
+           say = prev.charAt(j);
+           }else{
+           count++;
+           }
+       }
+       res = res + count + say;
+    }
+    return res;
+  }
+
+}
