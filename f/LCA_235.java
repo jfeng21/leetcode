691a1a54719ea20176507df727b811e9080a24b1
jfeng21 <junru.feng@asu.edu>
2016-07-11 19:26:53 -0400

    Update LCA_235.java

--- f/LCA_235.java
+++ f/LCA_235.java
@@ -1,18 +1,18 @@
 class LCA_235{
   public TreeNode lowestCommonAncester(TreeNode root, TreeNode p, TreeNode q){
-    if(root == null || root == p || root == q)
+    if(root == null || root == p || root == q)   
       return root;
    TreeNode left = lowestCommonAncester(root.left, p, q);
    TreeNode right = lowestCommonAncester(root.right, p, q);
    
-   if(left != null && right != null){
+   if(left != null && right != null){   //p and q might be in left and right
      return root;
-   }else if(left != null){
+   }else if(left != null){   //nodes in left
      return left;
-   }else if(right != null){
+   }else if(right != null){   //nodes in right
      return right;
    }else{
-   return null;
+   return null;   //null nodes
    }
   }
 
