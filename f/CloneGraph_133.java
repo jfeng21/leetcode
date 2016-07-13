9673d768204a9e8f1f02e05fa543213db44672e0
jfeng21 <junru.feng@asu.edu>
2016-07-13 15:01:54 -0400

    Update CloneGraph_133.java

--- f/CloneGraph_133.java
+++ f/CloneGraph_133.java
@@ -7,7 +7,8 @@ public UndirectedGraphNode cloneGraph(UndirectedGraphNode graph){
 }
 private UndirectedGraphNode dfs(UndirectedGraphNode graph, Map<UndirectedGraphNode, UndirectedGraphNode> map){
    if(graph == null) return null;
-    if(map.containsKey(graph))  return map.get(graph);
+    if(map.containsKey(graph)) //if the node is already exists in map, just get its neighbors
+       return map.get(graph);
     
     //copy nodes
     UndirectedGraphNode copy = new UndirectedGraphNode(graph.label);
@@ -20,26 +21,28 @@ private UndirectedGraphNode dfs(UndirectedGraphNode graph, Map<UndirectedGraphNo
     return copy;
 }
 
-//BFS
+//BFS--prefered
 public UndirectedGraphNode cloneGraph(UndirectedGraphNode graph){
   if(graph == null)  return null;
+  
   Map<UndirectedGraphNode, UndirectedGraphNode> map = new HashMap<>();
   Queue<UndirectedGraphNode> q = new LinkedList();
-  q.add(graph);
+  q.add(graph);  //offer the head node to queue
   UndirectedGraphNode copy = new UndirectedGraphNode(graph.label);
-  map.put(graph, copy);
+  map.put(graph, copy);   //mapping the graph node and its copy
   
   while(!q.isEmpty()){
     UndirectedGraphNode node = q.poll();
     //copy and connect neighbors
     for(UndirectedGraphNode neighbor : node.neighbors){
-       if(map.containsKey(neighbor)){
-         map.get(node).neighbors.add(map.get(neighbor));
-       }else{
+       if(map.containsKey(neighbor)){    //if the node is already in the map, just adds its neighbors
+         map.get(node).neighbors.add(map.get(neighbor));    //copy.neighbors.add(copied neighbor)
+       }else{   //if the node is not in the map, create new copy
         UndirectedGraphNode neighborCopy = new UndirectedGraphNode(neighbor.label);
+        //add neighbor copy and then mapping it 
         map.get(node).neighbors.add(neighborCopy);
         map.put(neighbor, neighborCopy);
-      //add neighbor to queue
+         //add neighbor to queue for future traverse
         q.add(neighbor);
        }
     }
