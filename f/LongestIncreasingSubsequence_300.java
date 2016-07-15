c6eba9bf4efa3234522c4e163c7282165f11a5fb
jfeng21 <junru.feng@asu.edu>
2016-07-15 18:30:08 -0400

    Update LongestIncreasingSubsequence_300.java

--- f/LongestIncreasingSubsequence_300.java
+++ f/LongestIncreasingSubsequence_300.java
@@ -1,5 +1,9 @@
 class LongestIncreasingSubsequence_300{
 //dp. O(n)
+//state: a[i]--prev i, ending with i's LIS
+//func: a[i] = max{a[j] + 1}, j < i && a[j] <= a[i]
+//initialize: f[0...n-1] = 1
+//ans: max(f[0...n-1])
   public int lengthOfLIS(int[] nums){
       if(nums.length == 0)  return 0;
       int[] a = new int[nums.length];
@@ -7,7 +11,7 @@ class LongestIncreasingSubsequence_300{
       for(int i = 0; i < nums.length; i++){
         a[i] = 1;  //initialize, longest subsequence at least one
         for(int j = 0; j < i; j++){
-          if(nums[i] > nums[j]){
+          if(nums[i] > nums[j]){  
              a[i] = a[i] > a[j] + 1 ? a[i] : a[j] + 1;
           }
         }
