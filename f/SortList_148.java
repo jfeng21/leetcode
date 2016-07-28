a849a0534f19dfa83d5f9116d24ce8541a387e93
jfeng21 <junru.feng@asu.edu>
2016-07-28 19:10:50 -0400

    Create SortList_148.java

---
+++ f/SortList_148.java
@@ -0,0 +1,38 @@
+class SortList_148{
+  public ListNode sortList(ListNode head){
+     if(head == null || head.next == null)
+        return head;
+     //cut the list to two halves using slow and fast nodes
+     ListNode prev = null, slow = head, fast = head;
+     while(fast != null && fast.next != null){
+         prev = slow;
+         slow = slow.next;
+         fast = fast.next.next;
+     }
+     prev.next = null;  //updating 
+     //sort each half
+     ListNode l1 = sortList(head);
+     ListNode l2 = sortList(slow);  //slow is in the middle pos
+     //merge
+    return merge(l1, l2);
+  }
+  
+  private ListNode merge(ListNode l1, ListNode l2){
+     ListNode l = new ListNode(0), p = l;
+     while(l1 != null && l2 != null){
+        if(l1.val < l2.val){
+          p.next = l1;
+          l1 = l1.next;
+          
+        }else{
+           p.next = l2;
+           l2 = l2.next;
+        }
+          p = p.next; 
+     }
+     if(l1 != null) p.next = l1;
+     if(l2 != null) p.next = l2;
+     return l.next;
+  }
+
+}
