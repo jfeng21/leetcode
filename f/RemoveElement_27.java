f68d7257039817bdc402f332f1fb4995e13e9328
jfeng21 <junru.feng@asu.edu>
2016-07-29 10:26:35 -0400

    Create RemoveElement_27.java

---
+++ f/RemoveElement_27.java
@@ -0,0 +1,17 @@
+class RemoveElement_27{
+   public int removeElement(int[] nums, int val){
+      int i = 0, n = nums.length;
+      while(i < n){
+         if(nums[i] == val){
+            nums[i] = nums[n - 1];
+            n--;
+            }
+         else
+            i++;
+            
+      }
+      return n;
+   }
+
+
+}
