b269a4518f22cf0fd1abed08d35c46e3fc333417
jfeng21 <junru.feng@asu.edu>
2016-07-29 11:43:02 -0400

    Create EvaluateReverse_150.java

---
+++ f/EvaluateReverse_150.java
@@ -0,0 +1,27 @@
+class EvaluateReverse_150{
+   public int evalRPN(String[] tokens){
+       int a, b;
+        Stack<Integer> stack = new Stack<Integer>();
+        for(String s : tokens){
+            if(s.equals("+")){
+                stack.add(stack.pop() + stack.pop());
+            }else if(s.equals("/")){
+                a = stack.pop();
+                b = stack.pop();
+                stack.add(b / a);
+                
+            }else if(s.equals("*")){
+                stack.add(stack.pop() * stack.pop());
+            }else if(s.equals("-")){
+                a = stack.pop();
+                b = stack.pop();
+                stack.add(b - a);
+            }else{
+                stack.add(Integer.parseInt(s));
+            }
+            
+        }
+        return stack.pop();
+   }
+
+}
