2c87a832e3ebd408856df01f1592270f1edc5275
jfeng21 <junru.feng@asu.edu>
2016-07-29 10:43:01 -0400

    Create ContainsDuplicate_217.java

---
+++ f/ContainsDuplicate_217.java
@@ -0,0 +1,12 @@
+class ContainsDuplicate_217{
+   public boolean containsDuplicate(int[] nums){
+      Set<Integer> set = new HashSet<>();
+      for(int x : nums){
+         if(set.contains(x))
+            return true;
+         set.add(x);
+      }
+      return false;
+   }
+
+}
