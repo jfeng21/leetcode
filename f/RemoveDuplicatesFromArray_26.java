dfa3e78895c0201ef6b12d097145237c36642182
jfeng21 <junru.feng@asu.edu>
2016-08-17 10:31:43 -0400

    Update RemoveDuplicatesFromArray_26.java

--- f/RemoveDuplicatesFromArray_26.java
+++ f/RemoveDuplicatesFromArray_26.java
@@ -2,14 +2,16 @@ class RemoveDuplicatesFromArray_26{
    public int removeDuplicates(int[] nums){
       int i = 0, n = nums.length;
       if(n = 0)  return 0;
-      
-      for(int j = 1; j < nums.length; j++){
-         if(nums[i] != nums[j]){
-           i++;
-           nums[i] = nums[j];
-         }
-      }
-      return i + 1;
-   }
-
+     
+       for(int j = 1; j < n; j++){
+          //if nums[i] == nums[j], move j to next element, then replace duplicated nums[i] with nums[j]
+          if(nums[j] != nums[i]){
+             i++;
+             nums[i] = nums[j];
+          }
+          
+       }
+       
+    return i + 1;   //since i starts from 0
+}
 }
